854a7423bcf3d92b6627fd87d9d4d6e86d412e6a
Will Constable <whc@fb.com>
2022-12-15 18:47:47 +0000

    Try to deal with calling .wait()
    ghstack-source-id: be23b7bed783b89cd97c0ab30c4c238ec79ee142
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90877

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -654,8 +654,8 @@ class Scheduler:
                     name_to_users[node1_name] = name_to_users[node2_name]
 
         def rename(n):
-            if n in self.mutation_renames:
-                return rename(self.mutation_renames[n])
+            # if n in self.mutation_renames:
+            # return rename(self.mutation_renames[n])
             return n
 
         def dep_closure(node_name):
@@ -1118,7 +1118,6 @@ class Scheduler:
                 ):
                     self.flush()
                     self.current_device = device
-
             self.buffer_names_to_free.update(node.last_usage)
 
             if node.is_template():
